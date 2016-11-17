92daf68911cd510de9804fb31ba953771dfe85f1
284794507 <284794507@qq.com>
2016-11-17 15:26:52 +0800

    C#面试切面编程简单实现方式

---
+++ AspectFTest/AspectFDemo/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspectFDemo
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Age { get; set; }
+        
+        public Action<string> WriterLog { get; set; }
+
+        public void Walk()
+        {
+            Console.WriteLine(this.Name + "Can walk!");
+        }
+
+        public void Run()
+        {
+            Dictionary<string, string> dictTmp = null;
+            AspectF.Define.Log(this.WriterLog, "开始跑步......", "跑步结束。")
+                .Retry()
+                .MustBeNonNull(dictTmp)
+                .Do(() =>
+                {
+                    Console.WriteLine(this.Name + "is running!");
+                    Thread.Sleep(1000);
+                    int[] arr = new int[0];
+                    arr[0]= 1;
+                    arr[1]= 2;
+                });
+        }
+    }
+}
---
+++ AspectFTest/AspectFDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Logger;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspectFDemo
+{
+    class Program
+    {
+        static SimpleLogger LFI_Log = SimpleLogger.GetInstance();
+
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "John";
+            SimpleLogger log= SimpleLogger.GetInstance();
+            log.Info("1231");
+            p.WriterLog = WriterLog;
+            p.Walk();
+            p.Run();
+
+            Console.ReadLine();
+        }
+
+        public static void WriterLog(string msg)
+        {
+            LFI_Log.Info(msg);
+            Console.WriteLine(msg);
+        }
+    }
+}
